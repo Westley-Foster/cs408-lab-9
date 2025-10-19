5b12089f5d90108caf93c0b65887c3d10c03b180
Westley Foster <westleyfoster@u.boisestate.edu>
2025-10-19 00:54:18 -0600

    Created Shape class. Started writing EvilCircle class (added constructor and EventListener)

--- js/main.js
+++ js/main.js
@@ -18,12 +18,58 @@ function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
 
-class Ball {
-  constructor(x, y, velX, velY, color, size) {
+//Shape class (had to be added above Ball class to work)
+class Shape {
+  constructor(x, y, velX, velY) {
     this.x = x;
     this.y = y;
     this.velX = velX;
     this.velY = velY;
+  }
+}
+
+//EvilCircle class (NOTE TO SELF: might be needed to be put below Ball)
+class EvilCircle extends Shape {
+  constructor(x, y) {
+    super(x, y, 20, 20); //passes up the x and y values to Shape and hardcodes velX and velY in shape to 20
+    //NOTE TO SELF: color and size might need to be changed to actually work, don't know yet)
+    this.color = white;
+    this.size = 10;
+
+    //adds an Event Listener to allow player movement
+    window.addEventListener("keydown", (e) => {
+      switch (e.key) {
+        case "a":
+          this.x -= this.velX; //moves character left
+          break;
+        
+        case "d":
+          this.x += this.velX; //moves character right
+          break;
+        
+        case "w":
+          this.y -= this.velY; //moves character up
+          break;
+
+        case "s":
+          this.y += this.velY; //moves character down
+          break;
+      }
+    });
+  }
+}
+
+class Ball extends Shape {
+  constructor(x, y, velX, velY, color, size) {
+    //my added stuff
+    super(x, y, velX, velY); //inherits the values x, y, velX, and velY from the Shape class
+    //NOTE TO SELF: might need to put exists below the other parameters color and size
+    this.exists = true; //creates a property "exists" that, if true, a ball has not been eaten yet
+
+    // this.x = x;
+    // this.y = y;
+    // this.velX = velX;
+    // this.velY = velY;
     this.color = color;
     this.size = size;
   }
@@ -57,14 +103,17 @@ class Ball {
   }
 
   collisionDetect() {
-    for (const ball of balls) {
-      if (!(this === ball)) {
-        const dx = this.x - ball.x;
-        const dy = this.y - ball.y;
-        const distance = Math.sqrt(dx * dx + dy * dy);
+    //detects a collision only if a ball exists
+    if(this.exists){
+      for (const ball of balls) {
+        if (!(this === ball)) {
+          const dx = this.x - ball.x;
+          const dy = this.y - ball.y;
+          const distance = Math.sqrt(dx * dx + dy * dy);
 
-        if (distance < this.size + ball.size) {
-          ball.color = this.color = randomRGB();
+          if (distance < this.size + ball.size) {
+            ball.color = this.color = randomRGB();
+          }
         }
       }
     }
